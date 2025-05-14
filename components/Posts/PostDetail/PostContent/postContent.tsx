38443b2aa19cc1a7ce7f077e6dd01bbe31d68b61
Vadim <_s_e_t__@ukr.net>
2025-05-14 16:14:11 +0300

    add gray-matter

--- components/Posts/PostDetail/PostContent/postContent.tsx
+++ components/Posts/PostDetail/PostContent/postContent.tsx
@@ -15,8 +15,8 @@ const PostContent = () => {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
   return (
     <article className={styles.content}>
-          <PostHeader title={DUMMY_POST.title} image={imagePath} />
-          {DUMMY_POST.content}
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 };
---
+++ content/getting-started-with-nextjs.md
@@ -0,0 +1,11 @@
+---
+title: 'Getting Started with NextJS'
+date: '2025-05-14'
+image: getting-started-nextjs.png
+description: NextJS is a the React framework
+isFeatured: true
+---
+
+# This is a title
+
+This is some regular text with a [link](https://google.com)
