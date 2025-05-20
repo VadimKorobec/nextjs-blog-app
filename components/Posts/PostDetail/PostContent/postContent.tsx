3a06dd79c4bbf9500878276e4ac2be0d3158bec1
Vadim <_s_e_t__@ukr.net>
2025-05-20 16:01:31 +0300

    add some code

--- components/Posts/PostDetail/PostContent/postContent.tsx
+++ components/Posts/PostDetail/PostContent/postContent.tsx
@@ -1,20 +1,39 @@
-import ReactMarkdown from 'react-markdown' 
+import ReactMarkdown from "react-markdown";
 
 import PostHeader from "../PostHeader/postHeader";
 import styles from "./postContent.module.css";
-import { Interface } from 'readline';
-import { Post } from '@/types/post';
+import { Post } from "@/types/post";
+import Image from "next/image";
+import { ComponentProps } from "react";
 
-interface PostContentProps{
-  post:Post
+interface PostContentProps {
+  post: Post;
 }
 
-const PostContent = ({post}:PostContentProps) => {
+const PostContent = ({ post }: PostContentProps) => {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    img: (props: ComponentProps<"img">) => {
+      const { src, alt } = props;
+
+      if (!src) return null;
+
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${src}`}
+          alt={alt || ""}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
