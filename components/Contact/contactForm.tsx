1e8d6cc94e87a8dc6fdefb18016bc2f87eda0032
Vadim <_s_e_t__@ukr.net>
2025-05-22 17:55:07 +0300

    add some code

--- components/Contact/contactForm.tsx
+++ components/Contact/contactForm.tsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
 import styles from "./contactForm.module.css";
+import { Message } from "@/types/message";
+
+const sendContactData = async (contactDetails: Message) => {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+};
 
 const ContactForm = () => {
   const [enteredEmail, setEnteredEmail] = useState<string>("");
   const [enteredName, setEnteredName] = useState<string>("");
   const [enteredMessage, setEnteredMessage] = useState<string>("");
+  const [requestStatus, setRequestStatus] = useState<
+    "pending" | "success" | "rejected" | null
+  >(null);
+  const [error, setError] = useState<null | string >(null);
 
   const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     const email = event.target.value;
@@ -23,20 +44,26 @@ const ContactForm = () => {
     setEnteredMessage(message);
   };
 
-  const handleSendMessage = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSendMessage = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify({
+    setRequestStatus("pending");
+
+    try {
+      await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      });
+      setRequestStatus("success");
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("Something went wrong");
+      } 
+      setRequestStatus("rejected");
+    }
 
     reset();
   };
@@ -46,6 +73,32 @@ const ContactForm = () => {
     setEnteredName("");
     setEnteredMessage("");
   };
+
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Sending message...",
+      message: "Your message is on its way!",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Success!",
+      message: "Message send successfully!",
+    };
+  }
+
+  if (requestStatus === "rejected") {
+    notification = {
+      status: "rejected",
+      title: "Success!",
+      message: "Message send successfully!",
+    };
+  }
 
   return (
     <section className={styles.contact}>
---
+++ components/Ui/notification.tsx
@@ -0,0 +1,30 @@
+import styles from "./notification.module.css";
+
+interface NotificationProps {
+  title: string;
+  message: string;
+  status: string;
+}
+
+const Notification = ({ title, message, status }: NotificationProps) => {
+  let statusClasses = "";
+
+  if (status === "success") {
+    statusClasses = styles.success;
+  }
+
+  if (status === "rejected") {
+    statusClasses = styles.error;
+  }
+
+  const cssClasses = `${styles.notification} ${statusClasses}`;
+
+  return (
+    <div className={cssClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Notification;
